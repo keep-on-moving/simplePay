acf173434eadc1e57efd8464036c40700df397e9
lihe <13718259084@163.com>
2019-08-16 11:00:11 +0800

    project init

---
+++ demon.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Payment\Wechat;
+
+Wechat::aaaaa();
---
+++ src/Payment/Wechat.php
@@ -0,0 +1,15 @@
+<?php
+namespace Payment;
+
+class Wechat
+{
+    public function __construct()
+    {
+
+    }
+
+    public static function aaaaa()
+    {
+        echo 111;
+    }
+}
